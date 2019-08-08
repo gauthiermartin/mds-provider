61993c1c257728a017f764508c3bc8c73c729822
Martin Gauthier <martin.gauthier@mindglass.io>
2019-08-08 14:16:22 -0400

    testing page behavior

--- mds/api/client.py
+++ mds/api/client.py
@@ -78,7 +78,7 @@ class ProviderClient():
             Checks if this :page: has a "data" property with a non-empty payload
             """
             data = page["data"] if "data" in page else {"__payload__": []}
-            links = page["links"]
+            print(page)
             payload = data[endpoint] if endpoint in data else []
             print(f"Got payload with {len(payload)} {endpoint}")
             print(links["next"])
